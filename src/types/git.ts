6c8ccfb74d5328610b4a83ac6f11704915756a2d
unknown <rektpunk@gmail.com>
2026-06-02 16:11:24 +0900

    remove dead code: Git Command

--- src/types/git.ts
+++ src/types/git.ts
@@ -20,12 +20,3 @@ export interface GitState {
   };
   openPrs: string[]; // List of branch names with open PRs
 }
-
-export type GitCommand =
-  | { type: "commit"; message: string }
-  | { type: "branch"; name: string; delete?: boolean }
-  | { type: "checkout"; target: string; isNew?: boolean }
-  | { type: "add"; files: string[] }
-  | { type: "push" }
-  | { type: "pull" }
-  | { type: "merge"; source: string };
